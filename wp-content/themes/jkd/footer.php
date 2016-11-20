e5590ab316cc29015f60cb3c428e73d6e2832609
kidionut <kidionut@gmail.com>
2016-11-20 08:51:04 +0200

    adeed the custom theme structure

---
+++ wp-content/themes/jkd/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package JKD
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jkd' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'jkd' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'jkd' ), 'jkd', '<a href="http://www.kovacsionut.com/" rel="designer">Kovacs Ionut</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
